ed790c19ad56b2d68308e70d05ae8d7aab90a0e1
Aleksa Cukovic <aleksacukovic1@gmail.com>
2021-05-31 00:23:05 +0200

    Fix import order rule

--- eslint-react/.eslintrc.js
+++ eslint-react/.eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
   root: true,
   extends: ["react-app"],
   rules: {
-    "import/order": ["error", { "newlines-between": "always" }],
+    "import/order": ["warn", { "newlines-between": "always" }],
   },
 };
